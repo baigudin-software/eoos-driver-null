beeb75a8abf32def78b80f783d27da9ad0124193
Sergey Baigudin <baigudin@mail.ru>
2023-12-25 00:29:19 +0300

    [EOOS-47] Controller creation bug fixed

--- source/drv.Null.cpp
+++ source/drv.Null.cpp
@@ -32,13 +32,17 @@ static NullController* getController()
     
 Null* Null::create(Config const& config)
 {
-    NullController* const controller( getController() );    
-    lib::UniquePointer<Null> res( controller->createResource(config) );
-    if( !res.isNull() )
-    {
-        if( !res->isConstructed() )
+    lib::UniquePointer<Null> res;
+    NullController* const controller( getController() );
+    if( controller != NULLPTR )
+    {    
+        res.reset( controller->createResource(config) );
+        if( !res.isNull() )
         {
-            res.reset();
+            if( !res->isConstructed() )
+            {
+                res.reset();
+            }
         }
     }
     return res.release();
