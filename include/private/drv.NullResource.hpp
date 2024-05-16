e43c6c7256f31a69dba4a24199694a0746307785
Sergey Baigudin <baigudin@mail.ru>
2024-05-17 01:14:40 +0300

    [EOOS-47] Driver fixed

--- include/private/drv.NullResource.hpp
+++ include/private/drv.NullResource.hpp
@@ -103,7 +103,7 @@ protected:
 private:
 
     /**
-     * Constructs this object.
+     * @brief Constructs this object.
      *
      * @return true if object has been constructed successfully.
      */
